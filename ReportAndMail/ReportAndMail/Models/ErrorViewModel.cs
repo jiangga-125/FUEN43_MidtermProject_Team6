feba48413914339e4ea5eb5b7eb066d06ebd6e4a
Rhys <hywgant@gmail.com>
2025-08-28 16:55:27 +0800

    08/28

---
+++ ReportAndMail/ReportAndMail/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ReportAndMail.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
